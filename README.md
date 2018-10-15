30ef1f9cc003fecbe8c2f8cca9d65e8e9eed42f4
YDKK <YDKK@users.noreply.github.com>
2018-10-15 19:39:27 +0900

    プロジェクト ファイルを追加します。

---
+++ README.md
@@ -0,0 +1,10 @@
+# iksm_refresher
+This app make a GET request to `https://app.splatoon2.nintendo.net/home` every hour to keep your `iksm_session` cookie staaaay fresh!
+
+## Usage
+1. Get your cookie from smartphone app and set it to `config.json`  
+   (You can use the [mitmproxy instructions](https://github.com/frozenpandaman/splatnet2statink/wiki/mitmproxy-instructions))
+1. Run ```iksm_refresher.exe```
+
+### Install as a service
+Run ```iksm_refresher.exe install``` in the privileged prompt.
---
+++ iksm_refresher/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Reactive.Linq;
+using Newtonsoft.Json;
+using Topshelf;
+
+namespace iksm_refresher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<Instance>(s =>
+                {
+                    s.ConstructUsing(name => new Instance());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                x.RunAsLocalSystem();
+                x.SetDescription("This is iksm_refresher service.");
+                x.SetDisplayName("iksm_refresher_service");
+            });
+        }
+    }
+
+    class Instance
+    {
+        private readonly dynamic _config;
+        private IDisposable _timer;
+
+        public void Start()
+        {
+            _timer = Observable.Timer(TimeSpan.Zero, TimeSpan.FromHours(1)).Subscribe(async x =>
+            {
+                using (var client = new HttpClient())
+                using (var request = new HttpRequestMessage(HttpMethod.Get, "https://app.splatoon2.nintendo.net/home"))
+                {
+                    request.Headers.Add("Cookie", $"iksm_session={_config.iksm_session}");
+                    await client.SendAsync(request);
+                }
+            });
+        }
+
+        public void Stop()
+        {
+            _timer.Dispose();
+        }
+
+        public Instance()
+        {
+            var configFile = File.ReadAllText("config.json");
+            _config = JsonConvert.DeserializeObject(configFile);
+        }
+    }
+}
